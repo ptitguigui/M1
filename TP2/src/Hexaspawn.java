7a9ae57745d8d7fd9bec45edd55422eac9245ab1
irakoze <francodavy.irakoze.etu@univ-lille1.fr>
2018-10-07 18:14:24 +0200

    display ok

--- TP2/src/Hexaspawn.java
+++ TP2/src/Hexaspawn.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
 
+
 public class Hexaspawn {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int y = sc.nextInt();
 		int x = sc.nextInt();
-
 		boolean[][] pionBlanc = new boolean[x][y];
 		boolean[][] pionNoir = new boolean[x][y];
-
 		initPlateau(sc, y, x, pionBlanc, pionNoir);
 		
 		System.out.println(displayTab(y, x, pionBlanc, pionNoir));
+		sc.close();
 	}
 
 	private static void initPlateau(Scanner sc, int y, int x, boolean[][] pionBlanc, boolean[][] pionNoir) {
+		String ligne = sc.nextLine();
 		for (int j = 0; j < y; j++) {
-			String ligne = sc.next();
+			ligne = sc.nextLine();
 			for (int i = 0; i < x; i++) {
-				if (ligne.equals('P')) {
+				if (ligne.charAt(i) == 'P') {
 					pionBlanc[i][j] = true;
 				} else if (ligne.charAt(i) == 'p') {
 					pionNoir[i][j] = true;
@@ -26,14 +27,15 @@ public class Hexaspawn {
 					pionBlanc[i][j] = false;
 					pionNoir[i][j] = false;
 				}
+				
 			}
 		}
 	}
 
 	private static String displayTab(int y, int x, boolean[][] pionBlanc, boolean[][] pionNoir) {
 		String res = "";
-		for (int i = 0; i < y; i++) {
-			for (int j = 0; j < x; j++) {
+		for (int j = 0; j < y; j++) {
+			for (int i = 0; i < x; i++) {
 				if (pionBlanc[i][j] == true) {
 					res += "P";
 				} else if (pionNoir[i][j] == true) {

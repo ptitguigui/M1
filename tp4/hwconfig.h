a65227cb4ae57b66463f3cf2061696747cbbd4e4
Irakoze Franco-davy <irakoze@a11p15.fil.univ-lille1.fr>
2018-10-10 11:06:42 +0200

    tp4 done'

---
+++ tp4/hwconfig.h
@@ -0,0 +1,12 @@
+#ifndef _HW_CONFIG_H_
+#define _HW_CONFIG_H_
+
+#define INIFILENAME	"hwconfig.ini"
+
+/* Horloge */
+#define TIMER_CLOCK	0xF0
+#define TIMER_PARAM     0xF4
+#define TIMER_ALARM     0xF8
+#define TIMER_IRQ	2   
+
+#endif

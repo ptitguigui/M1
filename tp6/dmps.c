d6e8b7a29facb80fc7976b091ce6ede76530c178
Lepretre Guillaume <lepretreg@a11p15.fil.univ-lille1.fr>
2018-10-24 10:28:49 +0200

    init tp6

---
+++ tp6/dmps.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "drive.h"
+
+static void empty_it()
+{
+    return;
+}
+
+int main(int argc, char **argv)
+{
+    unsigned int i, sectorsize, sector, cylinder;
+    unsigned char* buffRead;
+    unsigned char* buffWrite;
+    
+    /* init hardware */
+    if(init_hardware("hwconfig.ini") == 0) {
+	fprintf(stderr, "Error in hardware initialization\n");
+	exit(EXIT_FAILURE);
+    }
+
+    /* Interreupt handlers */
+    for(i=0; i<16; i++)
+	IRQVECTOR[i] = empty_it;
+
+    /* Allows all IT */
+    _mask(1);
+    
+    cylinder = 5;
+    sector = 5;
+    
+    _out(HDA_CMDREG,CMD_DSKINFO);
+    sectorsize=_in(HDA_DATAREGS+4)<<8;
+    sectorsize+=_in(HDA_DATAREGS+5);
+    buffRead=malloc(sectorsize);
+    buffWrite=malloc(sectorsize);
+    buffWrite = "test";
+    write_sector(cylinder, sector,10,buffWrite);
+    read_sector(cylinder,sector,buffRead);
+    dump(buffRead,sectorsize,1,1);
+    /*printf("\nDonnées du cylindre %d et de secteur %d\n", cylinder, sector);
+    printf("Entree : %s \n", buffWrite);
+    printf("Sortie : %s\n", buffRead);*/
+
+    /* and exit! */
+    exit(EXIT_SUCCESS);
+}

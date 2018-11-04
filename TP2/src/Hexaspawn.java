b306603ca638d53e0763188b00a43611eac1c2d8
Guillaume_LEPRETRE <lepretreg@guillaume.localdomain>
2018-11-04 16:44:40 +0100

    clean code

--- TP2/src/Hexaspawn.java
+++ TP2/src/Hexaspawn.java
@@ -1,20 +1,16 @@
-import java.awt.image.RescaleOp;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
 
+public class Hexaspawn {
+    private static Map<String, Integer> conf_map_n = new HashMap<String, Integer>();
+    private static Map<String, Integer> conf_map_b = new HashMap<String, Integer>();
 
-
-public class Hexaspawn{
-	private static Map<String,Integer> conf_map_n= new HashMap<String, Integer>();
-	private static Map<String,Integer> conf_map_b= new HashMap<String, Integer>();
     public static void main(String[] args) {
-    	ArrayList<Integer> conf = new ArrayList<Integer>();
-    	conf.add(null);
+        ArrayList<Integer> conf = new ArrayList<Integer>();
+        conf.add(null);
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
         int x = sc.nextInt();
@@ -98,44 +94,18 @@ public class Hexaspawn{
         ArrayList<Integer> conf = new ArrayList<Integer>();
         for (int i = 0; i < pionBlanc.length; i++) {
             for (int j = 0; j < pionBlanc[0].length; j++) {
-                    if (pionBlanc[i][j]) {
-                    	Object res = choixPionBlanc(i, j, pionBlanc, pionNoir);
-                    	if(res != null) {
-                    		conf.add((Integer) res);
-                    	}
+                if (pionBlanc[i][j]) {
+                    Object res = choixPionBlanc(i, j, pionBlanc, pionNoir);
+                    if (res != null) {
+                        conf.add((Integer) res);
                     }
                 }
-            
-        }
-        if(conf.isEmpty()) {
-        	return 0;
-        	
-        }
-        else {
-        	int min = 100000000;
-            boolean valeur_pos = false;
-        	for(int c : conf){
-        		if (c > 0){
-        			valeur_pos = true;
-        			if(c < min){
-                        min = c;
-                    }
-        		}
+            }
 
-        	}
-        	if(!valeur_pos){
-        		int min_ = Math.abs(conf.get(0));
-        		for(int c : conf){
-        			if (min_< Math.abs(c)){
-        				min_ = Math.abs(c);
-        			}
-        		}
-        		return -1 * min_;
-        	}
-        	return min;
         }
+        return getBetterConf(conf);
     }
-    
+
     /**
      * Methode qui fait jouer tous les pions d'une couleur noir selon leurtour
      *
@@ -146,42 +116,50 @@ public class Hexaspawn{
         ArrayList<Integer> conf = new ArrayList<Integer>();
         for (int i = 0; i < pionBlanc.length; i++) {
             for (int j = 0; j < pionBlanc[0].length; j++) {
-                	if(pionNoir[i][j]) {
-	                	Object res = choixPionNoir(i, j, pionBlanc, pionNoir);
-	                	if(res != null) {
-	                		conf.add((Integer)res);
-	                	}
-                
+                if (pionNoir[i][j]) {
+                    Object res = choixPionNoir(i, j, pionBlanc, pionNoir);
+                    if (res != null) {
+                        conf.add((Integer) res);
+                    }
+
                 }
             }
-            
+
         }
-        if(conf.isEmpty()) {
-        	return 0;
-        	
-        }
-        else {
-        	int min = 100000000;
+        return getBetterConf(conf);
+    }
+
+    /**
+     * Methode qui permet de renvoyer la meilleur configuration parmis une liste
+     * @param conf La list des configurations
+     * @return int la meilleur configuration
+     */
+    private static int getBetterConf(ArrayList<Integer> conf) {
+        if (conf.isEmpty()) {
+            return 0;
+
+        } else {
+            int min = 100000000;
             boolean valeur_pos = false;
-        	for(int c : conf){
-        		if (c > 0){
-        			valeur_pos = true;
-        			if(c < min){
+            for (int c : conf) {
+                if (c > 0) {
+                    valeur_pos = true;
+                    if (c < min) {
                         min = c;
                     }
-        		}
+                }
 
-        	}
-        	if(!valeur_pos){
-        		int min_ = Math.abs(conf.get(0));
-        		for(int c : conf){
-        			if (min_< Math.abs(c)){
-        				min_ = Math.abs(c);
-        			}
-        		}
-        		return -1 * min_;
-        	}
-        	return min;
+            }
+            if (!valeur_pos) {
+                int min_ = Math.abs(conf.get(0));
+                for (int c : conf) {
+                    if (min_ < Math.abs(c)) {
+                        min_ = Math.abs(c);
+                    }
+                }
+                return -1 * min_;
+            }
+            return min;
         }
     }
 
@@ -194,98 +172,100 @@ public class Hexaspawn{
      * @param pionNoir  le tableau de pion noir
      */
     private static Object choixPionBlanc(int i, int j, boolean[][] pionBlanc, boolean[][] pionNoir) {
-    	
+
         if (j > 0) {
-        	ArrayList<Integer> choix = new ArrayList<Integer>();
+            ArrayList<Integer> choix = new ArrayList<Integer>();
             // Peux avancer
-            if (!pionNoir[i][j - 1] && !pionBlanc[i][j - 1]){
+            if (!pionNoir[i][j - 1] && !pionBlanc[i][j - 1]) {
                 // Si il gagne en avancant
                 if (j == 1) {
-                	return 1;
+                    return 1;
 
                 } else {
                     // Sinon il avance
                     pionBlanc[i][j] = false;
                     pionBlanc[i][j - 1] = true;
                     String tab = displayTab(pionBlanc, pionNoir);
-                    if(!conf_map_b.containsKey(tab)) {
-                    int res = play_noir(pionBlanc, pionNoir);
-                    conf_map_b.put(tab, res);}
+                    if (!conf_map_b.containsKey(tab)) {
+                        int res = play_noir(pionBlanc, pionNoir);
+                        conf_map_b.put(tab, res);
+                    }
                     choix.add(conf_map_b.get(tab));
                     pionBlanc[i][j] = true;
                     pionBlanc[i][j - 1] = false;
                 }
             }
-            
+
             // Si peux manger en diagonale droite
             if (i < pionNoir.length - 1 && pionNoir[i + 1][j - 1]) {
                 // Si il gagne en mangeant
                 if (j == 1) {
-                	return 1;
+                    return 1;
                 } else {
                     pionNoir[i + 1][j - 1] = false;
                     pionBlanc[i + 1][j - 1] = true;
                     pionBlanc[i][j] = false;
                     String tab = displayTab(pionBlanc, pionNoir);
-                    if(!conf_map_b.containsKey(tab)) {
-                    int res = play_noir(pionBlanc, pionNoir);
-                    conf_map_b.put(tab, res);}
+                    if (!conf_map_b.containsKey(tab)) {
+                        int res = play_noir(pionBlanc, pionNoir);
+                        conf_map_b.put(tab, res);
+                    }
                     choix.add(conf_map_b.get(tab));
                     pionNoir[i + 1][j - 1] = true;
                     pionBlanc[i + 1][j - 1] = false;
                     pionBlanc[i][j] = true;
                 }
             }
-            
+
             // Si peux manger en diagonale gauche
             if (i > 0 && pionNoir[i - 1][j - 1]) {
                 // Si il gagne en mangeant
                 if (j == 1) {
-                	return 1;
+                    return 1;
                 } else {
                     pionNoir[i - 1][j - 1] = false;
                     pionBlanc[i - 1][j - 1] = true;
                     pionBlanc[i][j] = false;
                     String tab = displayTab(pionBlanc, pionNoir);
-                    if(!conf_map_b.containsKey(tab)) {
-                    int res = play_noir(pionBlanc, pionNoir);
-                    conf_map_b.put(tab, res);}
+                    if (!conf_map_b.containsKey(tab)) {
+                        int res = play_noir(pionBlanc, pionNoir);
+                        conf_map_b.put(tab, res);
+                    }
                     choix.add(conf_map_b.get(tab));
                     pionNoir[i - 1][j - 1] = true;
                     pionBlanc[i - 1][j - 1] = false;
                     pionBlanc[i][j] = true;
                 }
             }
-            
+
             // Si pion bloque
             if (choix.isEmpty()) {
-            	return null;
-            	
-            }
-            else if(!choix.isEmpty()) {
-            	int min = 100000000;
+                return null;
+
+            } else if (!choix.isEmpty()) {
+                int min = 100000000;
                 int res = 0;
-            	for(int c : choix) {
-            		if (c <= 0) {
-            			if (min > Math.abs(c)) {
+                for (int c : choix) {
+                    if (c <= 0) {
+                        if (min > Math.abs(c)) {
                             res = Math.abs(c) + 1;
                             min = Math.abs(c);
                         }
-            		}
+                    }
 
-            	}
-            	if(res == 0) {
-            		int max = choix.get(0);
-            		for(int c : choix) {
-            			if (max < c) {
-            				max = c;
-            			}
-            		}
-            		return -1 * (max + 1);
-            	}
-            	return res;
+                }
+                if (res == 0) {
+                    int max = choix.get(0);
+                    for (int c : choix) {
+                        if (max < c) {
+                            max = c;
+                        }
+                    }
+                    return -1 * (max + 1);
+                }
+                return res;
             }
-            
+
         }
         return 0;
     }
@@ -299,9 +279,9 @@ public class Hexaspawn{
      * @param pionNoir  le tableau de pion noir
      */
     private static Object choixPionNoir(int i, int j, boolean[][] pionBlanc, boolean[][] pionNoir) {
-    	
+
         if (j < pionBlanc[0].length - 1) {
-        	ArrayList<Integer> choix = new ArrayList<Integer>();
+            ArrayList<Integer> choix = new ArrayList<Integer>();
             // Peux avancer
             if (!pionBlanc[i][j + 1] && !pionNoir[i][j + 1]) {
                 // Si il gagne en avancant
@@ -312,35 +292,37 @@ public class Hexaspawn{
                     pionNoir[i][j] = false;
                     pionNoir[i][j + 1] = true;
                     String tab = displayTab(pionBlanc, pionNoir);
-                    if(!conf_map_n.containsKey(tab)) {
-                    int res = play_blanc(pionBlanc, pionNoir);
-                    conf_map_n.put(tab, res);}
+                    if (!conf_map_n.containsKey(tab)) {
+                        int res = play_blanc(pionBlanc, pionNoir);
+                        conf_map_n.put(tab, res);
+                    }
                     choix.add(conf_map_n.get(tab));
                     pionNoir[i][j] = true;
                     pionNoir[i][j + 1] = false;
                 }
             }
-            
+
             // Si peux manger en diagonale gauche
             if (i < pionBlanc.length - 1 && pionBlanc[i + 1][j + 1]) {
                 // Si il gagne en mangeant
                 if (j == pionNoir[0].length - 2) {
-                   return 1;
+                    return 1;
                 } else {
                     pionBlanc[i + 1][j + 1] = false;
                     pionNoir[i + 1][j + 1] = true;
                     pionNoir[i][j] = false;
                     String tab = displayTab(pionBlanc, pionNoir);
-                    if(!conf_map_n.containsKey(tab)) {
-                    int res = play_blanc(pionBlanc, pionNoir);
-                    conf_map_n.put(tab, res);}
+                    if (!conf_map_n.containsKey(tab)) {
+                        int res = play_blanc(pionBlanc, pionNoir);
+                        conf_map_n.put(tab, res);
+                    }
                     choix.add(conf_map_n.get(tab));
                     pionBlanc[i + 1][j + 1] = true;
                     pionNoir[i + 1][j + 1] = false;
                     pionNoir[i][j] = true;
                 }
             }
-            
+
             //Si peux manger en diagonale droite
             if (i != 0 && pionBlanc[i - 1][j + 1]) {
                 // Si il gagne en mangeant
@@ -351,9 +333,10 @@ public class Hexaspawn{
                     pionNoir[i - 1][j + 1] = true;
                     pionNoir[i][j] = false;
                     String tab = displayTab(pionBlanc, pionNoir);
-                    if(!conf_map_n.containsKey(tab)) {
-                    int res = play_blanc(pionBlanc, pionNoir);
-                    conf_map_n.put(tab, res);}
+                    if (!conf_map_n.containsKey(tab)) {
+                        int res = play_blanc(pionBlanc, pionNoir);
+                        conf_map_n.put(tab, res);
+                    }
                     choix.add(conf_map_n.get(tab));
                     pionBlanc[i - 1][j + 1] = true;
                     pionNoir[i - 1][j + 1] = false;
@@ -362,37 +345,36 @@ public class Hexaspawn{
 
             }
             // Pion bloque
-         // Si pion bloque
+            // Si pion bloque
             if (choix.isEmpty()) {
-            	return null;
-            	
-            }
-            else if(!choix.isEmpty()) {
-            	int min = 100000000;
+                return null;
+
+            } else if (!choix.isEmpty()) {
+                int min = 100000000;
                 int res = 0;
-            	for(int c : choix) {
-            		if (c <= 0) {
-            			if (min > Math.abs(c)) {
+                for (int c : choix) {
+                    if (c <= 0) {
+                        if (min > Math.abs(c)) {
                             res = Math.abs(c) + 1;
                             min = Math.abs(c);
                         }
-            		}
+                    }
 
-            	}
-            	if(res == 0) {
-            		int max = choix.get(0);
-            		for(int c : choix) {
-            			if (max < c) {
-            				max = c;
-            			}
-            		}
-            		return -1 * (max + 1);
-            	}
-            	return res;
+                }
+                if (res == 0) {
+                    int max = choix.get(0);
+                    for (int c : choix) {
+                        if (max < c) {
+                            max = c;
+                        }
+                    }
+                    return -1 * (max + 1);
+                }
+                return res;
             }
-            
+
         }
-		return 0;
+        return 0;
     }
 
 }

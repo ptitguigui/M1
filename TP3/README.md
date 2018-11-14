8486153aea430c50b05a660036177f73b9cd7769
Lepretre Guillaume <lepretreg@a13p15.fil.univ-lille1.fr>
2018-11-14 15:03:35 +0100

    partie 1

--- TP3/README.md
+++ TP3/README.md
@@ -14,3 +14,8 @@ Dans ce TP vous trouverez :
 ### Question 1 :
 
 Le certificat de notre problème est de faire une permutation des villes où la distance est inférieur à l. Etant donnée que nous avons n villes, la taille des certificats sera : `n log(n)`. Le taille de tous les certificats sera `n!` et donc polynomiale.
+
+### Question 2 : 
+
+<p>Q.1 Le schéma est le suivant : On choisis un certificat de façon aléatoire, nous calculons sa distance totale puis nous vérifions si il est valide, à savoir, si "distance total < k" </p> 
+ 
--- TP3/src/NP.java
+++ TP3/src/NP.java
@@ -1,55 +1,174 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class NP {
-	
+
 	static int betterSolution;
 	static int dimension;
 	static int[][] matrice;
-		
+	static int k = 40;
+
 	public static void main(String[] args) {
 		initValues();
+		
+		vericiationAllPossibilities(generateAllPossibilities());
+		
+		/*
+		List<Integer> alea = certificatAleatoire();
+		System.out.println(displayList(alea));
+		System.out.println("La distance de cette liste aleatoire : " + calculDistance(alea));
+		System.out.println("Le certificat aléatoire avec k=" + k + " a-t-il reussis ? " + verifierCertificat(alea, k));
+
+		List<Integer> verification = certificatVerification();
+		System.out.println("La distance de cette liste aleatoire : " + calculDistance(verification));
+		System.out.println(
+				"Le certificat aléatoire avec k=" + k + " a-t-il reussis ? " + verifierCertificat(verification, k));
+		*/
 	}
-	
+
 	static void initValues() {
 		Scanner sc = new Scanner(System.in);
 		skip(sc);
 		String line = sc.nextLine();
-		
-		String [] tab = line.split(" ");
-		String nb = tab[tab.length-1];
-		
+
+		String[] tab = line.split(" ");
+		String nb = tab[tab.length - 1];
+
 		dimension = Integer.parseInt(nb);
 		matrice = new int[dimension][dimension];
-		
+
 		skip(sc);
-		for(int i =0; i < dimension; i++) {
+		int cpt = 0;
+		for (int i = 0; i < dimension; i++) {
 			line = sc.nextLine();
-			String[] data= line.split(" ");
-			System.out.println("");
-			System.out.println(line);
+			String[] data = line.split(" ");
 			for (int j = 0; j < data.length; j++) {
-				if(!data[j].equals("") && !data[j].equals("\t")) {
-					System.out.println(data[j]+"test");
-					matrice[i][j] = Integer.parseInt(data[j]);
+				if (!data[j].trim().equals("")) {
+					matrice[i][cpt] = Integer.parseInt(data[j].trim());
+					cpt++;
 				}
 			}
+			cpt = 0;
 		}
-		display();
+		// display();
+	}
+
+	static List<Integer> certificatAleatoire() {
+		List<Integer> list = new ArrayList();
+		while (list.size() != dimension) {
+			Random random = new Random();
+			int nb = random.nextInt(dimension);
+			if (!list.contains(nb)) {
+				list.add(nb);
+			}
+		}
+		return list;
+	}
+
+	static List<Integer> certificatVerification() {
+		List<Integer> list = new ArrayList();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez renseigner un certificat de taille " + dimension);
+
+		String certificat = sc.nextLine();
+		String data[] = certificat.split(" ");
+
+		for (int i = 0; i < dimension; i++) {
+			list.add(Integer.parseInt(data[i]));
+		}
+
+		return list;
 	}
 	
+
+	static void vericiationAllPossibilities(List<List<Integer>> list) {
+		int total = list.size();
+		int totalTrue = 0;
+		int totalFalse = 0;
+		List<List<Integer>> listTrue= new ArrayList<List<Integer>>();
+		for (List<Integer> otherList : list) {
+			if(verifierCertificat(otherList, k)) {
+				totalTrue ++;
+				listTrue.add(otherList);
+			}else {
+				totalFalse ++;
+			}
+		}
+		System.out.println("Il y a un total de "+total+" certificats avec "+totalTrue+" true et "+totalFalse+" false pour k = "+k);
+		displayAllPossibilities(listTrue);
+	}
+
+	static List<List<Integer>> generatePerm(List<Integer> original) {
+		if (original.size() == 0) {
+			List<List<Integer>> result = new ArrayList<List<Integer>>();
+			result.add(new ArrayList<Integer>());
+			return result;
+		}
+		Integer firstElement = original.remove(0);
+		List<List<Integer>> returnValue = new ArrayList<List<Integer>>();
+		List<List<Integer>> permutations = generatePerm(original);
+		for (List<Integer> smallerPermutated : permutations) {
+			for (int index = 0; index <= smallerPermutated.size(); index++) {
+				List<Integer> temp = new ArrayList<Integer>(smallerPermutated);
+				temp.add(index, firstElement);
+				returnValue.add(temp);
+			}
+		}
+		return returnValue;
+	}
+
+	static int calculDistance(List<Integer> list) {
+		int res = 0;
+		for (int i = 0; i < list.size(); i++) {
+			res += matrice[list.get(i)][list.get((i + 1) % dimension)];
+		}
+		return res;
+	}
+
+	static boolean verifierCertificat(List<Integer> list, int k) {
+		return calculDistance(list) < k;
+	}
+	
+
 	static void display() {
 		for (int i = 0; i < dimension; i++) {
 			for (int j = 0; j < dimension; j++) {
-				System.out.print(matrice[i][j]+ " ");
+				System.out.print(matrice[i][j] + " ");
 			}
 			System.out.println("");
 		}
 	}
 
+	static String displayList(List<Integer> list) {
+		String res = "Voici la liste : ";
+		for (Integer integer : list) {
+			res += integer + " ";
+		}
+		return res;
+	}
+
+	static List<List<Integer>> generateAllPossibilities() {
+		List<Integer> listPossibilities = new ArrayList<Integer>();
+		for(int i=0; i< dimension; i++) {
+			listPossibilities.add(i);
+		}
+		
+		return generatePerm(listPossibilities);
+	}
+	static void displayAllPossibilities(List<List<Integer>> list) {
+		
+		for (List<Integer> otherList : list) {
+			System.out.println(displayList(otherList));
+		}
+	}
+
 	private static void skip(Scanner sc) {
-		for(int i = 0; i < 3; i++) {
+		for (int i = 0; i < 3; i++) {
 			sc.nextLine();
 		}
 	}
-	
+
 }

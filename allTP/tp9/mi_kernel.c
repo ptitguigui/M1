0fbab4dcfe89bbd49d156d460522cfe655a99f01
Irakoze Franco-davy <irakoze@a13p15.fil.univ-lille1.fr>
2018-11-28 13:23:34 +0100

    TP9

---
+++ allTP/tp9/mi_kernel.c
@@ -0,0 +1,76 @@
+#include "mi_syscall.h"
+
+unsigned int current_process = 0;
+
+static int ppage_of_vpage(int process, unsigned vpage){
+    if(vpage > N/2 -1){
+        return -1;
+    }
+
+    if(process == 0){
+        return vpage+1;
+    }else if(process == 1){
+        return vpage+N/2+1;
+    }else{
+        return -1;
+    }
+}
+
+
+static void switch_to_process0(void) 
+{
+    current_process = 0;
+    _out(MMU_CMD, MMU_RESET);
+}
+
+static void switch_to_process1(void) 
+{
+    current_process = 1;
+    _out(MMU_CMD, MMU_RESET);
+}
+
+static int error(){
+    printf("tentative d’accès illégal à l’adresse %p\n", _in(MMU_FAULT_ADDR));
+    exit(EXIT_FAILURE);
+}
+
+static void mmuhandler() 
+{
+    int vaddr;
+
+    vaddr = _in(MMU_FAULT_ADDR);
+
+    if((unsigned int)virtual_memory < vaddr || vaddr >= ((unsigned int) virtual_memory + VM_SIZE)){
+        printf("dedans premier if : VM_SIZE = 0x%.8X, virtual_memory = 0x%.8X, vaddr = 0x%.8X,  \n", VM_SIZE, virtual_memory, vaddr );
+        error();
+    }
+
+    int vpage = (vaddr >> 12) & 0xFFF;
+
+    if(vpage >= N/2){
+        printf("dedans deuxieme if \n");
+        error();
+    }
+
+    struct tlb_entry_s tlbe;
+    tlbe.tlbe_vpage = vpage;
+    tlbe.tlbe_ppage = ppage_of_vpage(current_process, vpage);
+    tlbe.tlbe_xwr = 7;
+    tlbe.tlbe_access = 1;
+    _out(TLB_ADD_ENTRY, *((int *)(&tlbe)));
+}
+
+void init(){
+     /* init hardware */
+    if(init_hardware("hardware.ini") == 0) {
+	    fprintf(stderr, "Error in hardware initialization\n");
+	    exit(EXIT_FAILURE);
+    }
+
+    /* Interreupt handlers */
+    IRQVECTOR[MMU_IRQ] = mmuhandler;
+    IRQVECTOR[SYSCALL_SWTCH_0] = switch_to_process0;
+    IRQVECTOR[SYSCALL_SWTCH_1] = switch_to_process1;
+    
+    
+}
---
+++ allTP/tp9/mi_syscall.h
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+#include "hardware.h"
+
+#define SYSCALL_SWTCH_0 16
+#define SYSCALL_SWTCH_1 17
+
+#define PAGE_SIZE 4096
+#define PM_PAGES 256
+#define VM_PAGES 4096
+#define PM_SIZE (PM_PAGES * PAGE_SIZE)
+#define VM_SIZE (VM_PAGES * PAGE_SIZE)
+
+#define N (PM_PAGES -2)
+
+#define MMU_IRQ 13
+#define MMU_CMD 0x66
+#define MMU_FAULT_ADDR 0xCD
+
+#define TLB_ADD_ENTRY	0xCE
+
+struct tlb_entry_s{
+    unsigned int tlbe_rfu:8;
+    unsigned int tlbe_vpage:12;
+    unsigned int tlbe_ppage:8;
+    unsigned int tlbe_xwr:3;
+    unsigned int tlbe_access:1;
+};
+
+void init(void);

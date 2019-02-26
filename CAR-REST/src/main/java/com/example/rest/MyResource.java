ba30f023cd6aa7885c3bc627fbb0891c448be5f6
Lepretre Guillaume <lepretreg@a11p15.fil.univ-lille1.fr>
2019-02-26 18:43:33 +0100

    test call REST request

--- CAR-REST/src/main/java/com/example/rest/MyResource.java
+++ CAR-REST/src/main/java/com/example/rest/MyResource.java
@@ -1,9 +1,15 @@
 package com.example.rest;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.net.ftp.FTPClient;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -11,15 +17,32 @@ import javax.ws.rs.core.MediaType;
 @Path("myresource")
 public class MyResource {
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
-    }
+	private String address;
+	private int port;
+	private FTPClient client;
+
+	public MyResource() throws Exception {
+		this.address = InetAddress.getLocalHost().getHostName();
+		this.port = 1025;
+		this.client = new FTPClient();
+	}
+
+	/**
+	 * Method handling HTTP GET requests. The returned object will be sent to the
+	 * client as "text/plain" media type.
+	 *
+	 * @return String that will be returned as a text/plain response.
+	 * @throws IOException
+	 * @throws Exception
+	 */
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getIt() throws Exception {
+		this.client.connect(address, port);
+		this.client.user("fr");
+		String answer = client.getReplyString();
+		this.client.disconnect();
+
+		return answer;
+	}
 }

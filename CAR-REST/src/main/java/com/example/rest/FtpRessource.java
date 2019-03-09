05b0fa5241a4a3db2ea23db370329146ca777d19
Guillaume_LEPRETRE <lepretreg@guillaume.localdomain>
2019-03-09 16:19:03 +0100

    can download file

--- CAR-REST/src/main/java/com/example/rest/FtpRessource.java
+++ CAR-REST/src/main/java/com/example/rest/FtpRessource.java
@@ -1,63 +1,89 @@
 package com.example.rest;
 
-import java.io.IOException;
-import java.net.InetAddress;
+import org.apache.commons.net.ftp.FTPClient;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.MediaType;
-
-import org.apache.commons.net.ftp.FTPClient;
+import javax.ws.rs.Produces;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Root ftp (exposed at "ftpRessource" path)
  */
 @Path("ftp")
 public class FtpRessource {
-	private String address;
-	private int port;
-	private String username;
-	private String password;
-	private FTPClient client;
+    private FTPClient ftp;
 
-	public FtpRessource() throws Exception {
-		this.address = InetAddress.getLocalHost().getHostName();
-		this.port = 1025;
-		this.client = new FTPClient();
+    public FtpRessource() throws Exception {
+        String address = "127.0.1.1";
+        int port = 1025;
+        String username = "guillaume";
+        String password = "mdp123";
+        Client client = new Client(address, port, username, password);
+        client.connect();
+        this.ftp = client.getClient();
+    }
 
-	}
+    @GET
+    @Path("syst")
+    /**
+     * Permet de lancer la commande SYST du server ftp
+     */
+    public String doSyst() throws IOException {
+        try {
+            this.ftp.syst();
+        } catch (Exception e) {
+            this.ftp.disconnect();
+            e.printStackTrace();
+        }
+        return this.ftp.getReplyString();
+    }
 
-	@POST
-	@Path("connect")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public String connect(User user) throws IOException {
-		try {
-			this.client.connect(address, port);
-			this.username = user.getUsername();
-			this.password = user.getPassword();
-			this.client.login(this.username, this.password);
-		} catch (Exception e) {
-			this.client.disconnect();
-			e.printStackTrace();
-		}
-		return this.client.getReplyString();
-	}
 
-	@GET
-	@Path("syst")
-	public String doSyst() throws IOException {
-		try {
-			this.client.connect(address, port);
-			this.client.login(this.username, this.password);
-			this.client.syst();
-		} catch (Exception e) {
-			this.client.disconnect();
-			e.printStackTrace();
-		}
-		return this.client.getReplyString();
-	}
+    @GET
+    @Produces("application/octet-stream")
+    @Path("download/{filename :.*}")
+    /**
+     * Permet de télécharger le fichier sélectionner par l'utilisateur
+     */
+    public String getFile(@PathParam("filename") String filename) throws Exception {
+        try {
+            String directory = System.getProperty("user.dir")+"/files/";
+            FileOutputStream fos = new FileOutputStream(directory+ filename);
+            ftp.enterLocalPassiveMode();
+            ftp.retrieveFile(filename, fos);
+            if (fos != null) {
+                fos.close();
+                return "fichier téléchargé avec succès";
+            }else{
+                return "Erreur lors du téléchargement du fichier :" + filename+ "Reponse du server :"+ftp.getReplyString();
+            }
+        } catch (Exception e) {
+           e.printStackTrace();
+        }finally {
+            ftp.disconnect();
+        }
+        return null;
+    }
 
+
+    @GET
+    @Path("quit")
+    /**
+     * Permet de lancer la commande QUIT du server ftp
+     */
+    public String doQuit() throws Exception {
+        try {
+            this.ftp.quit();
+            return this.ftp.getReplyString();
+        } catch (Exception e) {
+            this.ftp.disconnect();
+            e.printStackTrace();
+        } finally {
+            ftp.disconnect();
+        }
+        return null;
+    }
 }
--- CAR-REST/src/main/java/com/example/rest/MyResource.java
+++ CAR-REST/src/main/java/com/example/rest/MyResource.java
@@ -1,15 +1,10 @@
 package com.example.rest;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import org.apache.commons.net.ftp.FTPClient;
+import java.io.IOException;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -17,23 +12,21 @@ import org.apache.commons.net.ftp.FTPClient;
 @Path("myresource")
 public class MyResource {
 
-	
+    public MyResource() {
 
-	public MyResource() {
-		
-	}
+    }
 
-	/**
-	 * Method handling HTTP GET requests. The returned object will be sent to the
-	 * client as "text/plain" media type.
-	 *
-	 * @return String that will be returned as a text/plain response.
-	 * @throws IOException
-	 * @throws Exception
-	 */
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getIt() throws Exception {
-		return "Got it!";
-	}
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent to the
+     * client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     * @throws IOException
+     * @throws Exception
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() throws Exception {
+        return "Got it!";
+    }
 }

244c5c577f32a1b5fa2e5d1de53f8b01757e2e9a
irakoze <francodavy.irakoze.etu@univ-lille1.fr>
2019-03-16 21:57:01 +0100

    try test

--- CAR-REST/src/test/java/rest/resource/MyResourceTest.java
+++ CAR-REST/src/test/java/rest/resource/MyResourceTest.java
@@ -4,11 +4,15 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+
 import org.glassfish.grizzly.http.server.HttpServer;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import tp2.Main;
+
 import static org.junit.Assert.assertEquals;
 
 public class MyResourceTest {
@@ -18,6 +22,7 @@ public class MyResourceTest {
 
     @Before
     public void setUp() throws Exception {
+    	
         // start the server
         server = Main.startServer();
         // create the client

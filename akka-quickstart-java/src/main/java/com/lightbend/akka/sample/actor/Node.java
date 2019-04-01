e78c37e4817d86d7c8d4eb82eed50e3488763e1f
irakoze <francodavy.irakoze.etu@univ-lille1.fr>
2019-04-01 21:25:42 +0200

    update delete

--- akka-quickstart-java/src/main/java/com/lightbend/akka/sample/actor/Node.java
+++ akka-quickstart-java/src/main/java/com/lightbend/akka/sample/actor/Node.java
@@ -3,14 +3,12 @@ package com.lightbend.akka.sample.actor;
 import java.util.HashMap;
 import java.util.List;
 
-
 import com.lightbend.akka.sample.Printer.Greeting;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-
 
 public class Node extends AbstractActor {
 
@@ -74,7 +72,7 @@ public class Node extends AbstractActor {
 	}
 
 	/**
-	 * Le noeud courant recevnant un message, l'envoie à tout ses fils
+	 * Le noeud courant recevant un message, l'envoie à tout ses fils
 	 *
 	 * @return
 	 */
@@ -86,30 +84,8 @@ public class Node extends AbstractActor {
 						ActorRef actorRef = wtg.noeuds.get(fils.get(i));
 						wtg.Visited.replace(fils.get(i), true);
 						if (wtg.who.contains("delete")) {
-							String noeud = wtg.who.substring(wtg.who.length() - 1);
-							if (wtg.Data.get(fils.get(i)).contains(noeud)) {
-								if (wtg.Data.get(noeud).size() > 1) {
-									wtg.Data.get(fils.get(i)).set(wtg.Data.get(fils.get(i)).indexOf(noeud),
-											wtg.Data.get(noeud).get(0));
-									System.out.println("Le noeud_" + noeud + " est remplacé par le noeud_"
-											+ wtg.Data.get(noeud).get(0) + " dans la liste des noeuds relié au noeud_"
-											+ fils.get(i));
-								} else if (wtg.Data.get(noeud).size() == 1) {
-									wtg.Data.get(fils.get(i)).remove(noeud);
-									System.out.println("Le noeud_" + noeud
-											+ " est supprimé dans la liste des noeuds relié au noeud_" + fils.get(i));
-								}
-								if (wtg.noeuds.containsKey(noeud)) {
-									wtg.noeuds.remove(noeud);
-									System.out.println("Le noeud_" + noeud + " est supprimé dans la liste des noeuds");
-								}
-							}
-							if (!wtg.Visited.containsValue(false)) {
-								wtg.Data.remove(noeud);
-								wtg.Visited.remove(noeud);
-								System.out.println(
-										"Le noeud_ " + noeud + " n'est plus accessible dans les datas et Visited");
-							}
+							delete(wtg, i);
+						} else if (wtg.who.contains("add")) {
 
 						}
 						System.out.println(
@@ -128,11 +104,50 @@ public class Node extends AbstractActor {
 			} else {
 				this.msg = nameNoeud + ", " + wtg.who + "\n Le " + this.nameNoeud + " n'a pas de fils";
 				getSelf().tell(new Tell(), ActorRef.noSender());
+				if (wtg.who.contains("delete") && nameNoeud.contains(wtg.who.substring(wtg.who.length() - 1))) {
+					System.out.println("le systeme arrete l'acteur associé au noeud noeud_"
+							+ wtg.who.substring(wtg.who.length() - 1));
+					system.stop(getSelf());
+				}
+				
 			}
 
 		}).match(Tell.class, x -> {
 			printerActor.tell(new Greeting(msg), getSelf());
 		}).build();
+	}
+
+	/**
+	 * gère la suppression d'un noeud
+	 * 
+	 * @param wtg WhotoTell object
+	 * @param i
+	 */
+	private void delete(WhoToTell wtg, int i) {
+
+		String noeud = wtg.who.substring(wtg.who.length() - 1);
+		if (wtg.Data.get(fils.get(i)) != null) {
+			if (wtg.Data.get(fils.get(i)).contains(noeud)) {
+				if (wtg.Data.get(noeud).size() > 1) {
+					wtg.Data.get(fils.get(i)).set(wtg.Data.get(fils.get(i)).indexOf(noeud), wtg.Data.get(noeud).get(0));
+					System.out.println("Le noeud_" + noeud + " est remplacé par le noeud_" + wtg.Data.get(noeud).get(0)
+							+ " dans la liste des noeuds relié au noeud_" + fils.get(i));
+				} else if (wtg.Data.get(noeud).size() == 1) {
+					wtg.Data.get(fils.get(i)).remove(noeud);
+					System.out.println("Le noeud_" + noeud + " est supprimé dans la liste des noeuds relié au noeud_"
+							+ fils.get(i));
+				}
+				if (wtg.noeuds.containsKey(noeud)) {
+					wtg.noeuds.remove(noeud);
+					System.out.println("Le noeud_" + noeud + " est supprimé dans la liste des noeuds");
+				}
+			}
+		}
+		if (!wtg.Visited.containsValue(false)) {
+			wtg.Data.remove(noeud);
+			wtg.Visited.remove(noeud);
+			System.out.println("Le noeud_ " + noeud + " n'est plus accessible dans les datas et Visited");
+		}
 	}
 
 }

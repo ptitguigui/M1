5ebb52009f223d924012313e0cd312abbb628e1d
Irakoze Franco-davy <irakoze@a10p25.fil.univ-lille1.fr>
2019-05-09 11:46:39 +0200

    tp2 done

--- tp_pyqt2/exo2/Dessin.py
+++ tp_pyqt2/exo2/Dessin.py
@@ -12,11 +12,12 @@ class Dessin(QMainWindow):
                 self.setCentralWidget(self.canvas)
 
         def addMenuAction(self,img, nom, shortcut, tip):
-                newAct = QAction(QIcon(img), nom, self)
+                icon = QIcon(img)
+                newAct = QAction(icon, nom, self)
                 newAct.setShortcut( shortcut )
                 newAct.setToolTip(tip)
                 newAct.setStatusTip(tip)
-                newAct.setEnabled( True )
+                newAct.setCheckable(True)
                 return newAct 
 
         def open(self):
@@ -25,6 +26,11 @@ class Dessin(QMainWindow):
                 if color.isValid():
                         print(color.name())
                         self.canvas.color = color
+                        pix = QPixmap("color.jpg")
+                        pix.fill(color)
+                        self.fileToolBar.actions()[0].setIcon(QIcon(pix))
+                        
+
         def valuechange(self):
                 print("width changed to " + str(self.sl.value()))
                 self.canvas.width = self.sl.value()
@@ -32,20 +38,20 @@ class Dessin(QMainWindow):
                 self.canvas = CanvasDessin()
                 self.setCentralWidget(self.canvas)
                 self.sl.setValue(2)
+                pix = QPixmap("color.jpg")
+                pix.fill(self.canvas.color)
+                self.fileToolBar.actions()[0].setIcon(QIcon(pix))
+
 
         def addMenuBar(self):
-                bar = self.menuBar()
-                fichierMenu = bar.addMenu("Option")
+                self.fileToolBar = QToolBar("file")              
+                self.addToolBar(self.fileToolBar)
                 
-                newAct = self.addMenuAction("color.jpg", "Select Color", "Ctrl+l", "Select Color to Use")
-                
-                fichierMenu.addAction(newAct)
-                newAct.triggered.connect(self.open)
-                        
-
-                fileToolBar = QToolBar("file")
-                fileToolBar.addAction(newAct)
-                self.addToolBar(fileToolBar)
+                pix = QPixmap("color.jpg")
+                pix.fill(self.canvas.color)
+                newColor = self.addMenuAction(pix, "Select Color", "Ctrl+l", "Select Color to Use")
+                newColor.triggered.connect(self.open)
+                self.fileToolBar.addAction(newColor)
 
                 self.sl = QSlider(Qt.Horizontal)
                 self.sl.setMinimum(2)
@@ -54,13 +60,11 @@ class Dessin(QMainWindow):
                 self.sl.setTickPosition(QSlider.TicksBelow)
                 self.sl.setTickInterval(2)
                 self.sl.valueChanged.connect(self.valuechange)
-                fileToolBar.addWidget(self.sl)
+                self.fileToolBar.addWidget(self.sl)
 
                 self.b1 = QPushButton("Reset")
-                self.b1.setCheckable(True)
-                self.b1.toggle()
                 self.b1.clicked.connect(self.reset)
-                fileToolBar.addWidget(self.b1)
+                self.fileToolBar.addWidget(self.b1)
                 self.setStatusBar(QStatusBar())
 
 
